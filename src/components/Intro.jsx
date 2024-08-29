688f314138217a9233305be8186ad00039146584
ZakRab <107758804+ZakRab@users.noreply.github.com>
2024-08-29 14:53:57 -0500

    refactor: Update build assets and dependencies

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -5,22 +5,31 @@ import { motion } from "framer-motion";
 function Intro() {
   return (
     <motion.div>
-      <div className="h-screen flex flex-col justify-center fixed">
+      <div className="h-screen w-full flex flex-col justify-center fixed">
         <ParallaxText baseVelocity={2}>
           <span className="text-8xl">Zakaria Rab</span>
         </ParallaxText>
         <ParallaxText baseVelocity={-2}>
           <span className="text-8xl"> Rab Zakaria </span>
         </ParallaxText>
-        <motion.div>
-          <div className="flex ml-20 mt-20">
-            <p className="font-semibold text-3xl">
+        <motion.div
+          initial={{ opacity: 0 }}
+          transition={{ duration: 1 }}
+          whileInView={{ opacity: 1 }}
+        >
+          <div className="flex justify-center">
+            <p className="font-semibold text-3xl mt-3">
               Full-Stack and Mobile Software Engineer
+            </p>
+          </div>
+          <div>
+            <p className="font-semibold text-3xl mt-3 text-center">
+              React | React Native | Node.js | Express | MongoDB
             </p>
           </div>
         </motion.div>
       </div>
-      <div className="h-screen flex flex-col justify-center"></div>
+      <div className="h-screen"></div>
     </motion.div>
   );
 }
