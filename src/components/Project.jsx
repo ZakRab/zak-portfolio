1044f1382584a09c1b6d70e5d341f8e1f999e81b
ZakRab <107758804+ZakRab@users.noreply.github.com>
2024-09-12 15:14:40 -0500

    Refactor project and skills components

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -78,32 +78,33 @@ export function FeaturedProject({
   image,
 }) {
   return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      whileInView={{ opacity: 1 }}
-      transition={{ duration: 0.5 }}
-    >
-      <Link to="/tawasul">
-        <motion.div whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.8 }}>
-          <img
-            className="rounded-lg mb-10 border-2 border-gray-500"
-            src={image}
-            alt={image}
-          ></img>
-          <div className="flex flex-col gap-y-4 ">
-            <div className="flex w-full justify-between">
-              <p className="text-4xl font-semibold">{title}</p>
-
-              <div className="text-5xl font-medium flex gap gap-x-3">
-                {techStack.map((item) => {
-                  return skills[item];
-                })}
+    <div className="max-w-4xl">
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 0.5 }}
+      >
+        <Link to="/tawasul">
+          <motion.div whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.8 }}>
+            <p className="text-4xl font-semibold">{title}</p>
+            <img
+              className="rounded-lg my-10 border-2 border-gray-500"
+              src={image}
+              alt={image}
+            />
+            <div className="flex flex-col gap-y-4 ">
+              <div className="flex w-full justify-between">
+                <div className="text-5xl font-medium flex gap gap-x-3">
+                  {techStack.map((item) => {
+                    return skills[item];
+                  })}
+                </div>
               </div>
+              <p className="text-xl mt-4">{description}</p>
             </div>
-            <p className="text-2xl mt-4">{description}</p>
-          </div>
-        </motion.div>
-      </Link>
-    </motion.div>
+          </motion.div>
+        </Link>
+      </motion.div>
+    </div>
   );
 }
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -57,19 +57,56 @@ export const skills = {
   "Material UI": <SiMui />,
   Django: <SiDjango />,
 };
+const frontend = [
+  "Flutter",
+  "React",
+  "Svelte",
+  "HTML5",
+  "CSS3",
+  "Tailwind CSS",
+  "Bootstrap",
+  "Material UI",
+];
+const backend = [
+  "Node.js",
+  "SQL",
+  "Supabase",
+  "Dot Net",
+  "Express",
+  "MongoDB",
+  "Socket.io",
+  "Django",
+];
+const languages = ["Java", "JavaScript", "Dart", "C Sharp", "Python", "C"];
 function Skills() {
   return (
-    <div className="grid gap-y-20 lg:grid-cols-6 grid-cols-3 mt-20 mb-20">
-      {Object.keys(skills).map((skill) => {
-        return (
-          <>
-            <a data-tooltip-id="skills-tooltip" data-tooltip-content={skill}>
-              <Skill icon={skills[skill]} />
-            </a>
-            <Tooltip id="skills-tooltip" place="bottom-start" />
-          </>
-        );
-      })}
+    <div className="flex flex-col">
+      <SkillsSection heading="Frontend" sectionSkills={frontend} />
+      <SkillsSection heading={"Backend"} sectionSkills={backend} />
+      <SkillsSection heading={"Languages"} sectionSkills={languages} />
+    </div>
+  );
+}
+
+function SkillsSection({ heading, sectionSkills }) {
+  return (
+    <div className="mb-10">
+      <div className="text-3xl">{heading}</div>
+      <div className="flex flex-wrap justify-around">
+        {sectionSkills.map((sectionSkill) => {
+          return (
+            <>
+              <a
+                data-tooltip-id="skills-tooltip"
+                data-tooltip-content={sectionSkill}
+              >
+                <Skill icon={skills[sectionSkill]} />
+              </a>
+              <Tooltip id="skills-tooltip" place="bottom-start" />
+            </>
+          );
+        })}
+      </div>
     </div>
   );
 }
